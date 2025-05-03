bc6b212cec14b964bb7bafa15f607fd69ccf3e9b
Augusto <asergioscosta@gmail.com>
2025-05-03 15:03:02 -0300

    Criar Padrão Criacional Singleton e executar os testes.

---
+++ design-patterns/src/test/java/org/example/patterns/creational/singleton/ValidadorNotificacaoTest.java
@@ -0,0 +1,23 @@
+package org.example.patterns.creational.singleton;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ValidadorNotificacaoTest {
+
+    @Test
+    public void deveRetornarNomeValidador() {
+        ValidadorNotificacao.getInstance().setNomeValidador("Horário de Ponto");
+
+        assertEquals("Horário de Ponto", ValidadorNotificacao.getInstance().getNomeValidador());
+    }
+
+    @Test
+    public void deveRetornarEnvioNotificacao() {
+        ValidadorNotificacao.getInstance().setEnvioNotificacao("Ponto Registrado");
+
+        assertEquals("Ponto Registrado", ValidadorNotificacao.getInstance().getEnvioNotificacao());
+    }
+}
